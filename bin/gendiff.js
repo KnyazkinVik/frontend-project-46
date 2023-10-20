494388321340c49ecc62a407b75eb1c552dd8701
vknyazkin <vknyazkin@cs-cart.com>
2023-10-20 19:37:16 +0400

    Second task completed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ const program = new Command();
 
 program
   .name('gendiff')
-  .version('1.0.0');
+  .version('1.0.0')
+  .argument('filepath1')
+  .argument('filepath2')
+  .option('-f, --format <type>', 'output format')
 
 program.parse();
